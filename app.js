4e258c15b84d0f7ba6f00129d38d15578c94ac47
NK18OR <axel.leonelromano@gmail.com>
2021-09-28 10:25:23 -0300

    agregue el login y el register y termine de pulir algunas cosa de css

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ app.get("/", (req, res)=> {
     const archivoAEnviar = path.join(__dirname, "/views/home.html")
     res.sendFile(archivoAEnviar)
 })
+app.get("/register", (req, res)=> {
+    const archivoAEnviar = path.join(__dirname, "/views/register.html")
+    res.sendFile(archivoAEnviar)
+})
+app.get("/login", (req, res)=> {
+    const archivoAEnviar = path.join(__dirname, "/views/login.html")
+    res.sendFile(archivoAEnviar)
+})
 app.listen(3055, () => console.log( "Levantando un servidor con Express","http://localhost:3055/"));
